cdf229a45e96a5130a4f06b0bc15f2ed79ddccc2
aleesilva <aleesilva17@icloud.com>
2022-03-27 13:39:31 -0300

    initial commit, recursive challenger elixir returns a element length

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,8 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: length(list)
+
+  defp list_length([head | _tail]) do
+    list_length = head + 1
+    length(list_length)
   end
 end
